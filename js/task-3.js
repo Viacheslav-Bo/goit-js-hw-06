4ca7e377f0e75667bf20c0c7911d643aec56c2bf
Viacheslav-Bo-PC <zotikivna@gmail.com>
2025-10-02 23:42:21 +0300

    done

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,8 @@
 
 // Виконуй це завдання у файлі task-3.js
 
-// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок,
+// який записується у приватну властивість value об'єкта, що створюється.
 
 // Оголоси наступні методи класу:
 
@@ -12,11 +13,37 @@
 // padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
-// const builder = new StringBuilder(".");
-// console.log(builder.getValue()); // "."
-// builder.padStart("^");
-// console.log(builder.getValue()); // "^."
-// builder.padEnd("^");
-// console.log(builder.getValue()); // "^.^"
-// builder.padBoth("=");
-// console.log(builder.getValue()); // "=^.^="
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
